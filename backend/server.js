d41f9cc823cc1e3f44af0d3049b594aded111434
Gabriela Aparecida Emidio Simoes <1030491@senacminas.edu.br>
2026-06-24 16:53:48 -0300

    v1.2

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const db = mysql.createConnection({
     host: process.env.DB_HOST || "localhost",
     user: process.env.DB_USER || "root",
     password : process.env.DB_PASSWORD || "",
-    database : process.env.DB.NAME || "natacao",
+    database : process.env.DB_NAME || "natacao",
     port: process.env.DB_PORT || 3306,
     ssl: process.env.DB_HOST ? {
         rejectUnauthorized: false} : null
