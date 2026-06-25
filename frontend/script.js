715d3d7526aae14177dd7a5f97ad5ec030fd6b9f
Gabriela Aparecida Emidio Simoes <1030491@senacminas.edu.br>
2026-06-25 15:04:18 -0300

    v1.4

--- frontend/script.js
+++ frontend/script.js
@@ -1,6 +1,6 @@
 const API = window.location.hostname === "localhost" ||  window.location.hostname === "127.0.0.1"
 ? "http://localhost:3000"
-: "https://site-natacao-d85n.onrender.com";
+: "https://projeto-nata-o.onrender.com";
  
 async function cadastrarAluno(event) {
     event.preventDefault();
