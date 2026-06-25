8dc75e7ef3b17e84e91eae08303ea30c46fda8ba
Gabriela Aparecida Emidio Simoes <1030491@senacminas.edu.br>
2026-06-25 14:03:36 -0300

    v1.4

--- backend/server.js
+++ backend/server.js
@@ -1,39 +1,38 @@
 const express = require("express");
 const cors = require("cors");
-const mysql = require ("mysql2");
-
+const mysql = require("mysql2");
+ 
 const app = express();
 app.use(cors());
 app.use(express.json());
-
+ 
 const db = mysql.createConnection({
     host: process.env.DB_HOST || "localhost",
     user: process.env.DB_USER || "root",
-    password : process.env.DB_PASSWORD || "",
-    database : process.env.DB_NAME || "natacao",
+    password: process.env.DB_PASSWORD || "",
+    database: process.env.DB_NAME || "natacao",
     port: process.env.DB_PORT || 3306,
-    ssl: process.env.DB_HOST ? {
-        rejectUnauthorized: false} : null
+    ssl: process.env.DB_HOST ? { rejectUnauthorized: false} : null
 });
-
+ 
 db.connect((erro) => {
-    if(erro){
+    if(erro) {
         console.log("Erro ao conectar");
         console.log(erro);
         return;
     }
     console.log("Conectado com sucesso");
-    const criartabelaSQL = `
+    const criarTabelaSQL = `
     CREATE TABLE IF NOT EXISTS alunos (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(100) NOT NULL,
-    telefone INT NOT NULL,
+    idade INT NOT NULL,
     nivel VARCHAR(50) NOT NULL,
     horario VARCHAR(50) NOT NULL,
     ativo BOOLEAN DEFAULT TRUE
     );
     `;
-    db.query(criartabelaSQL, (erroTabela) => {
+    db.query(criarTabelaSQL, (erroTabela) => {
         if (erroTabela) {
             console.log("Erro de verificação ou criação da tabela", erroTabela);
         } else {
@@ -41,7 +40,7 @@ db.connect((erro) => {
         }
     })
 });
-
+ 
 app.get("/", (req, res) => {
     res.json({
         mensagem: "API funcionando"
@@ -57,7 +56,7 @@ app.post("/alunos", (req,res) => {
         return res.status(400).json({
             erro: "Preencha todos os campos."
         })
-    } 
+    }
  
         if (idade < 5) {
             return res.status(400).json({
@@ -65,101 +64,92 @@ app.post("/alunos", (req,res) => {
             })
         }
  
-        const verificaSQL = "SELECT * FROM alunos WHERE nome = ?"
-        db.query(verificaSQL, [nome], (erro, resultados) => {
-            if(erro){
-                return res.status(500).json(erro);
-            }
-        if (resultados.length > 0) {
-            return res.status(400).json({
-                erro: "Já existe um aluno com este nome"
+        const verificaSQL = "SELECT * FROM alunos WHERE nome = ?";
+        db.query(verificaSQL, [nome],
+            (erro, resultado) => {
+                if(erro) {
+                    return res.status(500).json(erro);
+                }
+                if (resultado.length > 0) {
+                    return res.status(400).json({
+                        erro: "Já existe este nome cadastrado no banco"
+                    })
+                }
+                const inserirSQL = `INSERT INTO alunos (nome, idade, nivel, horario) VALUES (?, ?, ?, ?)`;
+                db.query (inserirSQL, [nome, idade, nivel, horario], (erro, resultado) => {
+                    if (erro) {
+                        return res.status(500).json(erro);
+                    }
+                    res.status(201).json({
+                        mensagem: "Aluno cadstrado",
+                        id: resultado.insertId
+                    });
+                });
             });
-        }
-        const insertSQL = "INSERT INTO alunos (id, nome, idade, nivel, horario, ativo) VALUES (?, ?, ?, ?, ?, ?)";
-        db.query(insertSQL, [novoAluno.id, novoAluno.nome, novoAluno.idade, novoAluno.nivel, novoAluno.horario, novoAluno.ativo], (erro, resultados) => {
-            if(erro){
-                return res.status(500).json(erro);
-            }
-           if (resultados.length > 0) {
-            return res.status(400).json({
-                erro: "Erro ao cadastrar aluno."
-            })
-        }
-         const insertSQL = "INSERT INTO alunos (id, nome, idade, nivel, horario, ativo) VALUES (?, ?, ?, ?, ?, ?)";
-        db.query(insertSQL, [nome, idade, nivel, horario, ativo], (erro,resultado) => {
-            if(erro){ 
-                return res.status(500).json(erro);
-            }
-            res.status(201).json({
-                mensagem: "Aluno cadastrado com sucesso!",
-                id: resultado.insertId
-            });
-        });
     });
-});
  
     app.get("/alunos", (req,res) => {
-    db.query(
-        "SELECT * FROM alunos", (erro, resultados) => 
-            {
-            if(erro){
+        db.query(
+            "SELECT * FROM alunos", (erro, resultado) => {
+                if (erro) {
+                    return res.status(500).json(erro);
+                }
+                res.json(resultado);
+            });
+    });
+ 
+    app.delete("/alunos/:id", (req,res) => {
+        const id = req.params.id;
+        db.query( "DELETE FROM alunos WHERE id = ?", [id], (erro, resultado) => {
+            if(erro) {
                 return res.status(500).json(erro);
-            }
-            res.json(resultados);
-        }
-    );
-})
-        app.delete("/alunos/:id", (req,res) => {
-            const id = req.params.id
-        db.query("DELETE FROM alunos where id=?", [id],(erro, resultados) => {
-            if(erro){
-                return res.status(500).json(erro);
-            }
-            if (resultados.affectedRows === 0) {
+            } if  (resultado.affectedRows === 0) {
                 return res.status(404).json({
-                    erro:"Aluno não encontrado"
-                })
+                erro:"Aluno não encontrado"
+            })
             }
             res.json({
                 mensagem: "Aluno removido"
-            })
+            });
         });
-
-    })
+    });
  
     app.put("/alunos/:id", (req,res) => {
-        const id = (req.params.id)
-        db.query("SELECT ativos FROM alunos WHERE id=?", [id], (erro, resultados) => {
-       if(erro){
-        return res.status(500).json(erro);
-       }
-       if(resultados.length === 0){
-        return res.status(404).json({
-            erro:"Aluno não encontrado"
-        });
-       }
-         const novoStatus =
-         resultados[0].ativo === 1 ? 0 : 1;
-         db.query("UPDATE alunos SET ativo=? WHERE id=?", [novoStatus, id], (erro) => {
-            if(erro){
+        const id = req.params.id;
+        db.query("SELECT ativo FROM alunos WHERE id = ?", [id], (erro, resultado) => {
+            if (erro) {
                 return res.status(500).json(erro);
             }
-            res.json({
-                mensagem: "Status do aluno atualizado"
-            });
-         });
-    })
-});
+            if (resultado.length === 0) {
+                return res.status(404).json({
+                    erro: "Aluno não encontrado"
+                });
+            }
+            const novoStatus =
+            resultado[0].ativo ? 0 : 1;
  
-     let incorretas=0
-     let bloqueado= false
-
+            db.query("UPDATE alunos SET ativo = ? WHERE id = ?", [novoStatus, id], (erro) => {
+                if(erro) {
+                    return res.status(500).json(erro);
+                }
+                res.json({
+                    mensagem: "Aluno atualizado"
+                });
+            });
+        });
+    });
+ 
+ 
+ 
+    let incorretas = 0;
+    let bloqueado = false;
+ 
     app.post ("/admin", (req, res) => {
         const {senha} = req.body;
-        
-        if(bloqueado===true){
-            return res.status(403).json({
-                erro:"muitas tentativas. sistema bloqueado"
+ 
+        if(bloqueado===true) {
+            return res.status(403).json ({
+                erro: "Tentativas excedentes. Sistema bloqueado."
             });
         }
  
@@ -170,26 +160,23 @@ app.post("/alunos", (req,res) => {
         }
  
         if(senha === "admin123") {
-            incorretas=0
+            incorretas=0;
             return res.json({autenticado: true});
         }
         incorretas++;
-        if(incorretas>=3){
-            bloqueado=true;
+        if(incorretas>=3) {
+            bloqueado = true;
             return res.status(403).json({
-                erro:"sistema bloqueado"
-            })
+                erro: "Sistema bloqueado"
+            });
         }
-
-
         return res.status(401).json({
-            erro: `Senha incorreta. Faltam ${3-incorretas} ate o sistema bloquear`
+            erro: `Senha incorreta. Faltam ${3-incorretas} até o bloqueio do sistema.`
         });
     });
-
- const PORT = process.env.PORT ||3000;
+ 
+    const PORT = process.env.PORT || 3000;
     app.listen(PORT, () => {
         console.log("Servidor rodando em: ")
         console.log(`porta ${PORT}`)
-        })       
-    })         
+    })
